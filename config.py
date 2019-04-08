95fd53da1aee626d30065450e72f70226afc9afd
Suchist <37686503+Suchist@users.noreply.github.com>
2019-04-08 13:15:19 +0800

    创建config.py
    创建config.py用来配置sqlAchemy的环境，可以修改使用的数据库，我使用的是mysql.

---
+++ config.py
@@ -0,0 +1,11 @@
+#encoding:utf-8
+import os
+
+DEBUG=True
+SECRET_KEY=os.urandom(24)
+
+DB_URI = "mysql+mysqldb://root:123456@localhost:3306/publishsystem?charset=utf8"
+SQLALCHEMY_DATABASE_URI = DB_URI
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
