e2168d81dbc075de91183517d90b94fdf19b088b
Suchist <37686503+Suchist@users.noreply.github.com>
2019-04-22 09:16:19 +0800

    change database from mysql to sqlite

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ import os
 DEBUG=True
 SECRET_KEY=os.urandom(24)
 
-DB_URI = "mysql+mysqldb://root:123456@localhost:3306/publishsystem?charset=utf8"
+#DB_URI = "mysql+mysqldb://root:123456@localhost:3306/publishsystem?charset=utf8"
+DB_URI = "sqlite:///publishsystem.db"
 SQLALCHEMY_DATABASE_URI = DB_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
