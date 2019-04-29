e9aa3465f04df340506d9dd048b84bc8ec58d396
Suchist <37686503+Suchist@users.noreply.github.com>
2019-04-29 16:50:33 +0800

    Add new function in check.py to check  whether contains inappropriate words in article.

--- check.py
+++ check.py
@@ -5,3 +5,15 @@ def validateEmail(email):
             return 1
     return 0
 
+#inappropriate words
+    #1.定义inappropriate words数组
+    #2.遍历传进来的字符串
+    #3.return value
+dirtyword =  {'fuck','shite'}
+def inDictElement(term, dirtyword):
+    num_texts_with_term = len([True for text in dirtyword.values() if term.lower() in text.lower().split()])
+    if num_texts_with_term == len(dirtyword):
+        return 1
+    else:
+        return 0
+
